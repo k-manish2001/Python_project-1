4d58e2846969c32f9c0fc904d4add6b306b99f73
i@mankit <54908657+ankit9968@users.noreply.github.com>
2021-11-28 01:38:06 +0530

    proj 1 completed

---
+++ frontend.js
@@ -0,0 +1,9 @@
+function fetch(){
+    var master_path=document.getElementById("master").value;
+    var response_path=document.getElementById("response").value;
+    var pos_marks=document.getElementById("pos").value;
+    var neg_marks=document.getElementById("neg").value;
+    document.write(master_path+response_path+pos_marks+neg_marks);
+   
+    
+}
